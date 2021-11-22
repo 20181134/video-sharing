c0473ff9b607dc796ce1c508df48a98a25455aa3
20181134 <91303066+20181134@users.noreply.github.com>
2021-11-22 12:06:47 +0900

    Search page

--- search.php
+++ search.php
@@ -1,1 +1,13 @@
-
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Search</title>
+    </head>
+    <body>
+        <h1>Search</h1>
+        <form action="search-output.php">
+            Search by word: <input type="text" name="word">
+            <input type="submit" value="Search">
+        </form>
+    </body>
+</html>
